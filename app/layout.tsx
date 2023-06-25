8c627bc24fdc93d815248e05b0f1d5949f28be36
Steven Tey <stevensteel97@gmail.com>
2023-06-25 13:02:43 -0500

    edited title

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,7 @@ import { ReactNode } from "react";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "ChatHN",
+  title: "ChatHN – Chat with Hacker News using natural language",
   description:
     "Chat with Hacker News using natural language. Built with OpenAI Functions and Vercel AI SDK.",
 };
