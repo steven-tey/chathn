0de80d53989279365f7681cbfc1d6d0524fcdb9a
Steven Tey <stevensteel97@gmail.com>
2023-06-25 17:02:30 -0500

    added links

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ import { useRef } from "react";
 import { useChat } from "ai/react";
 import va from "@vercel/analytics";
 import clsx from "clsx";
+import { VercelIcon, GithubIcon, LoadingCircle, SendIcon } from "./icons";
 import { Bot, User } from "lucide-react";
 import ReactMarkdown from "react-markdown";
 import remarkGfm from "remark-gfm";
@@ -35,6 +36,22 @@ export default function Chat() {
 
   return (
     <main className="flex flex-col items-center justify-between pb-40">
+      <div className="absolute top-5 hidden w-full justify-between px-5 sm:flex">
+        <a
+          href="/deploy"
+          target="_blank"
+          className="rounded-lg p-2 transition-colors duration-200 hover:bg-stone-100 sm:bottom-auto"
+        >
+          <VercelIcon />
+        </a>
+        <a
+          href="/github"
+          target="_blank"
+          className="rounded-lg p-2 transition-colors duration-200 hover:bg-stone-100 sm:bottom-auto"
+        >
+          <GithubIcon />
+        </a>
+      </div>
       {messages.length > 0 ? (
         messages.map((message, i) => (
           <div
@@ -222,41 +239,3 @@ export default function Chat() {
     </main>
   );
 }
-
-const LoadingCircle = () => {
-  return (
-    <svg
-      aria-hidden="true"
-      className="h-4 w-4 animate-spin fill-stone-600 text-stone-200"
-      viewBox="0 0 100 101"
-      fill="none"
-      xmlns="http://www.w3.org/2000/svg"
-    >
-      <path
-        d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
-        fill="currentColor"
-      />
-      <path
-        d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
-        fill="currentFill"
-      />
-    </svg>
-  );
-};
-
-const SendIcon = ({ className }: { className?: string }) => {
-  return (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      viewBox="0 0 16 16"
-      fill="none"
-      className={className}
-      strokeWidth="2"
-    >
-      <path
-        d="M.5 1.163A1 1 0 0 1 1.97.28l12.868 6.837a1 1 0 0 1 0 1.766L1.969 15.72A1 1 0 0 1 .5 14.836V10.33a1 1 0 0 1 .816-.983L8.5 8 1.316 6.653A1 1 0 0 1 .5 5.67V1.163Z"
-        fill="currentColor"
-      ></path>
-    </svg>
-  );
-};
--- next.config.js
+++ next.config.js
@@ -3,6 +3,20 @@ const nextConfig = {
   experimental: {
     serverActions: true,
   },
+  redirects: async () => {
+    return [
+      {
+        source: "/github",
+        destination: "https://github.com/steven-tey/chathn",
+        permanent: true,
+      },
+      {
+        source: "/deploy",
+        destination: "https://vercel.com/templates/next.js/chathn",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
