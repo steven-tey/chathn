182bb938f4091273bfd4a4744a17a8b09f50ec9e
Rohan Jain <87605664+rohanikshvaku@users.noreply.github.com>
2023-08-06 15:29:31 +0530

    Update route.ts
    Added error handling to see why sometimes the responses fail or errors occur.

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -1,10 +1,10 @@
 import { kv } from "@vercel/kv";
 import { Ratelimit } from "@upstash/ratelimit";
-import { Configuration, OpenAIApi } from "openai-edge";
 import { OpenAIStream, StreamingTextResponse } from "ai";
 import { functions, runFunction } from "./functions";
+import { Configuration } from "openai-edge";
+import { OpenAIApi } from "openai-edge";
 
-// Create an OpenAI API client (that's edge friendly!)
 const config = new Configuration({
   apiKey: process.env.OPENAI_API_KEY,
 });
@@ -13,20 +13,15 @@ const openai = new OpenAIApi(config);
 export const runtime = "edge";
 
 export async function POST(req: Request) {
-  if (
-    process.env.NODE_ENV !== "development" &&
-    process.env.KV_REST_API_URL &&
-    process.env.KV_REST_API_TOKEN
-  ) {
+  // Rate limit check
+  if (process.env.NODE_ENV !== "development" && process.env.KV_REST_API_URL && process.env.KV_REST_API_TOKEN) {
     const ip = req.headers.get("x-forwarded-for");
     const ratelimit = new Ratelimit({
       redis: kv,
       limiter: Ratelimit.slidingWindow(50, "1 d"),
     });
 
-    const { success, limit, reset, remaining } = await ratelimit.limit(
-      `chathn_ratelimit_${ip}`,
-    );
+    const { success, limit, reset, remaining } = await ratelimit.limit(`chathn_ratelimit_${ip}`);
 
     if (!success) {
       return new Response("You have reached your request limit for the day.", {
@@ -40,61 +35,57 @@ export async function POST(req: Request) {
     }
   }
 
-  const { messages } = await req.json();
+  try {
+    const { messages } = await req.json();
+    const initialResponse = await openai.createChatCompletion({
+      model: "gpt-4-0613",
+      messages,
+      functions,
+      function_call: "auto",
+    });
+    const initialResponseJson = await initialResponse.json();
+    console.log("OpenAI Response:", initialResponseJson);
 
-  // check if the conversation requires a function call to be made
-  const initialResponse = await openai.createChatCompletion({
-    model: "gpt-3.5-turbo-0613",
-    messages,
-    functions,
-    function_call: "auto",
-  });
-  const initialResponseJson = await initialResponse.json();
-  const initialResponseMessage = initialResponseJson?.choices?.[0]?.message;
+    if (!initialResponseJson.choices || !initialResponseJson.choices.length) {
+      throw new Error("No choices in the response from OpenAI");
+    }
 
-  let finalResponse;
-
-  if (initialResponseMessage.function_call) {
-    const { name, arguments: args } = initialResponseMessage.function_call;
-    const functionResponse = await runFunction(name, JSON.parse(args));
-
-    finalResponse = await openai.createChatCompletion({
-      model: "gpt-3.5-turbo-0613",
-      stream: true,
-      messages: [
-        ...messages,
-        initialResponseMessage,
-        {
-          role: "function",
-          name: initialResponseMessage.function_call.name,
-          content: JSON.stringify(functionResponse),
+    const initialResponseMessage = initialResponseJson.choices[0].message;
+    
+    // Check for function call
+    if (initialResponseMessage.function_call) {
+      const { name, arguments: args } = initialResponseMessage.function_call;
+      const functionResponse = await runFunction(name, JSON.parse(args));
+      const finalResponse = await openai.createChatCompletion({
+        model: "gpt-4-0613",
+        stream: true,
+        messages: [
+          ...messages,
+          initialResponseMessage,
+          {
+            role: "function",
+            name: initialResponseMessage.function_call.name,
+            content: JSON.stringify(functionResponse),
+          },
+        ],
+      });
+      return new StreamingTextResponse(OpenAIStream(finalResponse));
+    } else {
+      const chunks = initialResponseMessage.content.split(" ");
+      const stream = new ReadableStream({
+        async start(controller) {
+          for (const chunk of chunks) {
+            const bytes = new TextEncoder().encode(chunk + " ");
+            controller.enqueue(bytes);
+            await new Promise((resolve) => setTimeout(resolve, Math.floor(Math.random() * 20 + 10)));
+          }
+          controller.close();
         },
-      ],
-    });
-    // Convert the response into a friendly text-stream
-    const stream = OpenAIStream(finalResponse);
-    // Respond with the stream
-    return new StreamingTextResponse(stream);
-  } else {
-    // if there's no function call, just return the initial response
-    // but first, we gotta convert initialResponse into a stream with ReadableStream
-    const chunks = initialResponseMessage.content.split(" ");
-    const stream = new ReadableStream({
-      async start(controller) {
-        for (const chunk of chunks) {
-          const bytes = new TextEncoder().encode(chunk + " ");
-          controller.enqueue(bytes);
-          await new Promise((r) =>
-            setTimeout(
-              r,
-              // get a random number between 10ms and 30ms to simulate a random delay
-              Math.floor(Math.random() * 20 + 10),
-            ),
-          );
-        }
-        controller.close();
-      },
-    });
-    return new StreamingTextResponse(stream);
+      });
+      return new StreamingTextResponse(stream);
+    }
+  } catch (error) {
+    console.error("Error in POST:", error);
+    return new Response("Internal Server Error", { status: 500 });
   }
 }
