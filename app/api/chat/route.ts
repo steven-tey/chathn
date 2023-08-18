481a52d120c521c7cde2fd2e6f42d24194c18122
Max Leiter <max.leiter@vercel.com>
2023-08-17 19:29:06 -0700

    Use experimental_onFunctionCall and OpenAI SDK

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -1,14 +1,16 @@
 import { kv } from "@vercel/kv";
 import { Ratelimit } from "@upstash/ratelimit";
-import { Configuration, OpenAIApi } from "openai-edge";
-import { OpenAIStream, StreamingTextResponse } from "ai";
+import { OpenAI } from "openai";
+import {
+  OpenAIStream,
+  StreamingTextResponse,
+} from "ai";
 import { functions, runFunction } from "./functions";
 
 // Create an OpenAI API client (that's edge friendly!)
-const config = new Configuration({
+const openai = new OpenAI({
   apiKey: process.env.OPENAI_API_KEY,
 });
-const openai = new OpenAIApi(config);
 
 export const runtime = "edge";
 
@@ -43,58 +45,28 @@ export async function POST(req: Request) {
   const { messages } = await req.json();
 
   // check if the conversation requires a function call to be made
-  const initialResponse = await openai.createChatCompletion({
+  const initialResponse = await openai.chat.completions.create({
     model: "gpt-3.5-turbo-0613",
     messages,
+    stream: true,
     functions,
     function_call: "auto",
   });
-  const initialResponseJson = await initialResponse.json();
-  const initialResponseMessage = initialResponseJson?.choices?.[0]?.message;
 
-  let finalResponse;
+  const stream = OpenAIStream(initialResponse, {
+    experimental_onFunctionCall: async (
+      { name, arguments: args },
+      createFunctionCallMessages,
+    ) => {
+      const result = await runFunction(name, args);
+      const newMessages = createFunctionCallMessages(result);
+      return openai.chat.completions.create({
+        model: "gpt-3.5-turbo-0613",
+        stream: true,
+        messages: [...messages, ...newMessages],
+      });
+    },
+  });
 
-  if (initialResponseMessage.function_call) {
-    const { name, arguments: args } = initialResponseMessage.function_call;
-    const functionResponse = await runFunction(name, JSON.parse(args));
-
-    finalResponse = await openai.createChatCompletion({
-      model: "gpt-3.5-turbo-0613",
-      stream: true,
-      messages: [
-        ...messages,
-        initialResponseMessage,
-        {
-          role: "function",
-          name: initialResponseMessage.function_call.name,
-          content: JSON.stringify(functionResponse),
-        },
-      ],
-    });
-    // Convert the response into a friendly text-stream
-    const stream = OpenAIStream(finalResponse);
-    // Respond with the stream
-    return new StreamingTextResponse(stream);
-  } else {
-    // if there's no function call, just return the initial response
-    // but first, we gotta convert initialResponse into a stream with ReadableStream
-    const chunks = initialResponseMessage.content.split(" ");
-    const stream = new ReadableStream({
-      async start(controller) {
-        for (const chunk of chunks) {
-          const bytes = new TextEncoder().encode(chunk + " ");
-          controller.enqueue(bytes);
-          await new Promise((r) =>
-            setTimeout(
-              r,
-              // get a random number between 10ms and 30ms to simulate a random delay
-              Math.floor(Math.random() * 20 + 10),
-            ),
-          );
-        }
-        controller.close();
-      },
-    });
-    return new StreamingTextResponse(stream);
-  }
+  return new StreamingTextResponse(stream);
 }
